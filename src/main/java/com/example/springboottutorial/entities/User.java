d33a1834fda2c7321d1c16e68536618438de9d78
Gunther Haas <guntherhaas.eng@gmail.com>
2022-08-17 20:48:57 -0300

    create class order

--- src/main/java/com/example/springboottutorial/entities/User.java
+++ src/main/java/com/example/springboottutorial/entities/User.java
@@ -2,10 +2,12 @@ package com.example.springboottutorial.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name="users")
+@Table(name="tb_user")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +17,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -64,6 +69,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders(){
+        return orders;
     }
 
     @Override
